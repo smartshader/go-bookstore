0a02afada71b23942613e82157706e3d8fb9a27a
Imad Jerra <imad.jerra@ariv.ca>
2021-01-06 18:06:12 -0500

    Insert users in database

--- users/domain/users/user_dao.go
+++ users/domain/users/user_dao.go
@@ -2,9 +2,16 @@ package users
 
 import (
 	"fmt"
+	"strings"
 
+	"smartshader/go-bookstore/users/datasources/mysql/users_db"
 	"smartshader/go-bookstore/users/utils/date"
 	"smartshader/go-bookstore/users/utils/errors"
+)
+
+const (
+	queryInsertUser  = "INSERT INTO users(first_name, last_name, email, date_created) VALUES (?, ?, ?, ?);"
+	indexUniqueEmail = "email_UNIQUE"
 )
 
 var (
@@ -27,16 +34,30 @@ func (user *User) Get() *errors.RestErr {
 }
 
 func (user *User) Save() *errors.RestErr {
-	current := usersDB[user.Id]
-	if current != nil {
-		if current.Email == user.Email {
-			return errors.NewBadRequestError(fmt.Sprintf("email %s already registered", user.Email))
-		}
-		return errors.NewBadRequestError(fmt.Sprintf("user %d already exists", user.Id))
+	stmt, err := users_db.Client.Prepare(queryInsertUser)
+	if err != nil {
+		return errors.NewInternalServerError(err.Error())
 	}
+	defer stmt.Close()
 
 	user.DateCreated = date.GetNowString()
 
-	usersDB[user.Id] = user
+	insertResult, err := stmt.Exec(user.FirstName, user.LastName, user.Email, user.DateCreated)
+	if err != nil {
+		if strings.Contains(err.Error(), indexUniqueEmail) {
+			return errors.NewBadRequestError(
+				fmt.Sprintf("email %s already exists", user.Email))
+		}
+		return errors.NewInternalServerError(
+			fmt.Sprintf("error when trying to save user: %s", err.Error()))
+	}
+
+	userId, err := insertResult.LastInsertId()
+	if err != nil {
+		return errors.NewInternalServerError(
+			fmt.Sprintf("error when trying to save user: %s", err.Error()))
+	}
+
+	user.Id = userId
 	return nil
 }
